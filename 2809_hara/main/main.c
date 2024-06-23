314a8f0882df72c0498de3ea2522ef1e6f5c36e9
unknown <noj7413@naver.com>
2024-06-23 20:46:21 +0900

    Jun 23, 2024. Tested IQmathLIb v1.5a. It worked well.

--- 2809_hara/main/main.c
+++ 2809_hara/main/main.c
@@ -56,15 +56,17 @@ void main(void)
 	System_Init();
 	Variable_Init();
 
-
+/*
 	for(i = 0; i < 360; i++)
 	{
-		th = _IQ17mpyIQX(th, 17, _IQ23div(_IQ23(3.14), _IQ23(180.0)), 23);
+		th = _IQ17cos(_IQ17mpyIQX(_IQ17(i), 17, _IQ17div(_IQ17(3.14), _IQ17(180.0)), 17));
 		test_iq17 = _IQ17acos(th);
-		TxPrintf("%lf\n", _IQ17toF(test_iq17));
+		TxPrintf("%ld, %lf, %lf\n", i, _IQ17toF(test_iq17), _IQ17toF(th));
+		DELAY_US(10000);
 	}
 
 	while(1);
+*/
 // condition is FALSE -> measure the sensor data to calculate dist
 #if 1
 	Menu();
